86e9e87b15dcbfd798bc55f9341bd9b4e23fe139
mumu2511 <109407300+mumu2511@users.noreply.github.com>
2022-09-13 13:43:36 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.20.0
-export FRPS_INIT="https://github.com/mumu2511/abc/blob/main/frps/frps.init"
+export FRPS_INIT="https://raw.githubusercontent.com/mumu2511/abc/main/frps/frps.init"
 export aliyun_download_url="https://github.com/mumu2511/abc/releases/download/01"
 export github_download_url="https://github.com/mumu2511/abc/releases/download/01"
