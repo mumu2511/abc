b8957b26addcc5d9d72f1641ee1433c5259414c8
mumu2511 <109407300+mumu2511@users.noreply.github.com>
2022-09-13 13:03:48 +0800

    Add files via upload

---
+++ frps/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPS_VER=0.20.0
+export FRPS_INIT="https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/frps.init"
+export aliyun_download_url="https://github.com/mumu2511/abc/tree/main/frp"
+export github_download_url="https://github.com/mumu2511/abc/tree/main/frp"
